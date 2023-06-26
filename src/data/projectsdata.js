fd0ba5f43c16811a3020f4d65b9749f6cc4f5768
Jhonson Marlon <111826126+Jhonsonmarlon@users.noreply.github.com>
2023-06-26 18:58:37 -0400

    update2

--- src/data/projectsdata.js
+++ src/data/projectsdata.js
@@ -4,7 +4,7 @@ export const projects = [
     description:
       "Um site de gerenciamento de eventos do instituto federal de roraima - (trabalho disc. DB1)",
     image:
-      "https://jhonsonmarlon.com.br/assets/imgs/proj11.jpg",
+      "https://jhonsonmarlon.com.br/assets/imgs/proj22.jpg",
     link: "https://database-ifrr.jhonsonmarlon.com.br/",
     subtitle: "HTML, PHP, CSS",
   },
@@ -22,7 +22,7 @@ export const projects = [
     description:
       "Desenvolvimento de uma pagina de login para o IEL",
     image:
-      "https://jhonsonmarlon.com.br/assets/imgs/proj22.jpg",
+      "https://jhonsonmarlon.com.br/assets/imgs/proj11.jpg",
     link: "https://jhonsonmarlon.github.io/PageLogin_IEL-RR/",
     subtitle: "HTML, Javascript, CSS",
   },
