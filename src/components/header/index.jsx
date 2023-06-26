1ef820eff5ac30a208ea476ebd62f94fc927fc26
Jhonson Marlon <111826126+Jhonsonmarlon@users.noreply.github.com>
2023-06-26 12:50:22 -0300

    back

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Background, BackgroundFade, Content } from "./styles";
 
 const backgroundLink =
-  "https://cdn2.unrealengine.com/Unreal+Engine%2Fdeveloper-interviews%2Fdeveloper-eggnut-highlights-how-they-created-backbone-s-dystopian-noir-2-5d-pixel-art-style%2FFB_EggNut_Backbone_Interview-1200x630-aed8f94ab90306fe6a4ecad0ebfc8ca519c926dc.jpg";
+  "https://escolaeducacao.com.br/wp-content/uploads/2021/07/curso-gratuito-programacao-750x430.jpg";
 
 const Header = () => {
   return (
