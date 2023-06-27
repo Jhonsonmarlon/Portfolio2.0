2566ceff592a6a73b548cb1878d0d6d0214c8ef2
Jhonson Marlon <111826126+Jhonsonmarlon@users.noreply.github.com>
2023-06-27 15:26:29 -0300

    updateteste

--- src/data/projectsdata.js
+++ src/data/projectsdata.js
@@ -4,7 +4,7 @@ export const projects = [
     description:
       "Um site de gerenciamento de eventos do instituto federal de roraima - (trabalho disc. DB1)",
     image:
-      "https://srv892-files.hstgr.io/f2a89219399b5fc4/api/preview/big/public_html/database-ifrr/assets/proj22.jpg?auth=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJsb2NhbGUiOiJwdF9CUiIsInZpZXdNb2RlIjoibGlzdCIsInNpbmdsZUNsaWNrIjpmYWxzZSwicGVybSI6eyJhZG1pbiI6ZmFsc2UsImV4ZWN1dGUiOmZhbHNlLCJjcmVhdGUiOnRydWUsInJlbmFtZSI6dHJ1ZSwibW9kaWZ5Ijp0cnVlLCJkZWxldGUiOnRydWUsInNoYXJlIjpmYWxzZSwiZG93bmxvYWQiOnRydWV9LCJjb21tYW5kcyI6W10sImxvY2tQYXNzd29yZCI6dHJ1ZSwiaGlkZURvdGZpbGVzIjpmYWxzZSwiZGF0ZUZvcm1hdCI6ZmFsc2V9LCJpc3MiOiJGaWxlIEJyb3dzZXIiLCJleHAiOjE2ODc4OTcyMjksImlhdCI6MTY4Nzg5MDAyOX0.gxsaZ0RxC9vFfrWR97Tu0PAKo7x99DABU5Um5ALbi18&inline=true&key=1687882098547",
+      "./proj22(1).jpg",
     link: "https://database-ifrr.jhonsonmarlon.com.br/",
     subtitle: "HTML, PHP, CSS",
   },
@@ -22,7 +22,7 @@ export const projects = [
     description:
       "Desenvolvimento de uma pagina de login para o IEL",
     image:
-      "https://jhonsonmarlon.com.br/public_html/assets/imgs/proj11.jpg",
+      "./proj11(1).jpg",
     link: "https://jhonsonmarlon.github.io/PageLogin_IEL-RR/",
     subtitle: "HTML, Javascript, CSS",
   },
