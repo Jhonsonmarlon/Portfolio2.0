fa3e6a553ae70cb90219245f97acd22d6b421a97
Jhonson Marlon <111826126+Jhonsonmarlon@users.noreply.github.com>
2023-06-28 12:30:16 -0300

    Update globalStyles.js

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,9 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-   @import url('https://fonts.googleapis.com/css2?family=Jost:wght@600;700&display=swap');
-
-    *{
+       *{
         font-family: 'Poppins';
         color: ${(props) => props.theme.text};
         padding: 0;
