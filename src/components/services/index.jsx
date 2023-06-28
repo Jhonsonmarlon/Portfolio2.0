21891a56bc6f759c91b09c5b17c571a3562c5c31
Jhonson Marlon <111826126+Jhonsonmarlon@users.noreply.github.com>
2023-06-28 11:33:33 -0300

    Update index.jsx

--- src/components/services/index.jsx
+++ src/components/services/index.jsx
@@ -6,7 +6,7 @@ import { BiCodeBlock } from "react-icons/bi";
 
 const Services = () => {
   return (
-    <Container>
+    <Container style="user-select: none;">
       <h1>Serviços</h1>
       <p>Um poucos do serviços que tenho a oferecer!</p>
       <Content>
