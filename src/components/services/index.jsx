7d7beb29a0d4e5b0cbbbfd6528fc0c3b261564de
Jhonson Marlon <111826126+Jhonsonmarlon@users.noreply.github.com>
2023-06-28 11:41:32 -0300

    Update index.jsx

--- src/components/services/index.jsx
+++ src/components/services/index.jsx
@@ -7,13 +7,13 @@ import { BiCodeBlock } from "react-icons/bi";
 const Services = () => {
   return (
     <Container>
-      <h1 style="user-select: none;">Serviços</h1>
-      <p style="user-select: none;">Um poucos do serviços que tenho a oferecer!</p>
+      <h1>Serviços</h1>
+      <p>Um poucos do serviços que tenho a oferecer!</p>
       <Content>
         <Card>
           <RiPenNibFill size={"100px"} />
-          <h2 style="user-select: none;">Analista de Dados</h2>
-          <p style="user-select: none;">
+          <h2>Analista de Dados</h2>
+          <p>
             Desenvolvo fórmulas ou alternativas demonstrativas para,
             representação de dados e estatísticas, fazendo avaliação
             de projetos.
@@ -21,16 +21,16 @@ const Services = () => {
         </Card>
         <Card>
           <BiCodeBlock size={"100px"} />
-          <h2 style="user-select: none;">Front-End</h2>
-          <p style="user-select: none;">
+          <h2 >Front-End</h2>
+          <p>
             Desenvolvo sites profissionais, blogs, portfólios,
             aplicativos, landing pages e e-commerce.
           </p>
         </Card>
         <Card>
           <RiServerFill size={"100px"} />
-          <h2 style="user-select: none;">Backend</h2>
-          <p style="user-select: none;">
+          <h2 >Backend</h2>
+          <p>
             Desenvolvo toda parte do lado do servidor, com banco de dados e
             criação de Apis para alimentar a aplicação web.
           </p>
