eeba11d1d4af6ed4c1156e69f2c80ef529a42799
Jhonson Marlon <111826126+Jhonsonmarlon@users.noreply.github.com>
2023-11-07 10:02:04 -0400

    Bg_tela_home.jsx

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Background, BackgroundFade, Content } from "./styles";
 
 const backgroundLink =
-  "https://escolaeducacao.com.br/wp-content/uploads/2021/07/curso-gratuito-programacao-750x430.jpg";
+  "https://firebasestorage.googleapis.com/v0/b/jhonsonportfolio.appspot.com/o/imgs_port%2Fbackground.jpg?alt=media&token=1ebdbaf2-3a64-4ccf-900a-f97203414464&_gl=1*6fcu6a*_ga*MjAxMjA3Nzc0NS4xNjk2MjczNDAz*_ga_CW55HF8NVT*MTY5OTM2NDA1Mi4xOC4xLjE2OTkzNjU2NzAuNTYuMC4w";
 
 const Header = () => {
   return (
