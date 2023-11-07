1ba0a7d2a6a0b482630c295d31136a253c9309dd
Jhonson Marlon <111826126+Jhonsonmarlon@users.noreply.github.com>
2023-11-07 10:09:18 -0400

    imgs_projects.js

--- src/data/projectsdata.js
+++ src/data/projectsdata.js
@@ -4,7 +4,7 @@ export const projects = [
     description:
       "Um site de gerenciamento de eventos do instituto federal de roraima - (trabalho disc. DB1)",
     image:
-      "https://github.com/Jhonsonmarlon/Portfolio2.0/raw/main/assetsgeral/fundo.jpg",
+      "https://firebasestorage.googleapis.com/v0/b/jhonsonportfolio.appspot.com/o/imgs_port%2Ffundo.jpg?alt=media&token=1f61f5da-6c01-49c5-9982-48509bdbf565&_gl=1*1yhpidt*_ga*MjAxMjA3Nzc0NS4xNjk2MjczNDAz*_ga_CW55HF8NVT*MTY5OTM2NDA1Mi4xOC4xLjE2OTkzNjYwNDAuMjQuMC4w",
     link: "https://database-ifrr.jhonsonmarlon.com.br/",
     subtitle: "HTML, PHP, CSS",
   },
@@ -13,7 +13,7 @@ export const projects = [
     description:
       "Um site para administração dos jogos da copa do mundo 2022",
     image:
-      "https://github.com/Jhonsonmarlon/CALENDARIO_DE_JOGOS-COPA2022/raw/main/assets/github/capa_ofc.jpg",
+      "https://firebasestorage.googleapis.com/v0/b/jhonsonportfolio.appspot.com/o/imgs_port%2Fimg-works2.svg?alt=media&token=7f2657af-e52c-4592-ae41-665081880365&_gl=1*zfruxs*_ga*MjAxMjA3Nzc0NS4xNjk2MjczNDAz*_ga_CW55HF8NVT*MTY5OTM2NDA1Mi4xOC4xLjE2OTkzNjYwNzkuNTkuMC4w",
     link: "https://jhonsonmarlon.github.io/CALENDARIO_DE_JOGOS-COPA2022/",
     subtitle: "React, JS, HTML",
   },
@@ -22,7 +22,7 @@ export const projects = [
     description:
       "Desenvolvimento de uma pagina de login para o IEL",
     image:
-      "https://github.com/Jhonsonmarlon/Portfolio2.0/raw/main/assetsgeral/proj11.jpg",
+      "https://firebasestorage.googleapis.com/v0/b/jhonsonportfolio.appspot.com/o/imgs_port%2Fproj11.jpg?alt=media&token=d16b72f5-ef8c-4ef0-b6e6-42ccbc15cc88&_gl=1*btsk6w*_ga*MjAxMjA3Nzc0NS4xNjk2MjczNDAz*_ga_CW55HF8NVT*MTY5OTM2NDA1Mi4xOC4xLjE2OTkzNjYxMTUuMjMuMC4w",
     link: "https://jhonsonmarlon.github.io/PageLogin_IEL-RR/",
     subtitle: "HTML, Javascript, CSS",
   },
@@ -31,7 +31,7 @@ export const projects = [
     description:
       "Plataforma de apostas criada alusivamente para a copaMUNDI",
     image:
-      "https://github.com/Jhonsonmarlon/Portfolio2.0/raw/main/assetsgeral/img-works1.svg",
+      "https://firebasestorage.googleapis.com/v0/b/jhonsonportfolio.appspot.com/o/imgs_port%2Fimg-works1.svg?alt=media&token=8b4fe681-606e-4c7e-922d-c5d7d87e5748&_gl=1*mjgw5l*_ga*MjAxMjA3Nzc0NS4xNjk2MjczNDAz*_ga_CW55HF8NVT*MTY5OTM2NDA1Mi4xOC4xLjE2OTkzNjYxMzAuOC4wLjA.",
     link: "https://github.com/Jhonsonmarlon/CursoWEBcompleto-natrave",
     subtitle: "HTML, Javascript, CSS",
   },
